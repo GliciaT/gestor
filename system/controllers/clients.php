ae7055c2ee52218e8b352fa1e2c1912271037c7a
Vitória Mendes <vivi.pena.mendes@gmail.com>
2017-04-24 23:06:50 -0300

    Iniciando teste no controller Clients

--- system/controllers/clients.php
+++ system/controllers/clients.php
@@ -6,18 +6,29 @@ class Clients extends Controller
 	{
 		parent::__construct();
 		$this->load_model('clients_model');
-    //$this->load_lib('test_lib'); aqui você só declara se for usar msm a biblioteca, agora não será necessário
 	}
 	public function test(){
 		$cliente = array('name' => 'Vitória', 'ddd_1' => '81', 'tel_primary' => '111111111', 'ddd_2' => '81', 'tel_optional' => '222222222', 'email' => 'email@teste');
 		$this->adicionarCliente($cliente);
+		$this->exibirCliente($cliente);
 	}
-  public function adicionarCliente($data=""){
+
+	public function adicionarCliente($data=""){
 		$this->model['cliente_model']->insert(CLIENTS_NAME, $data);
   }
+
 	public function alterarCliente(){
+		$this->model['cliente_model']->update(CLIENTS_NAME,$data,"WHERE id = '" .$data['id']. "'");
   }
+
 	public function removerCliente(){
+		$this->model['cliente_model']->delete(CLIENTS_NAME,$data,"WHERE id = '" .$data['id']. "'");
   }
+
+	public function exibirCliente($data=""){
+    $this->model['cliente_model']->select(CLIENTS_NAME,"WHERE id='" . $data['id']. "'");
+      $cliente = $this->model['cliente_model']->get_result();
+      var_dump($cliente);
+    }
 }
 ?>
