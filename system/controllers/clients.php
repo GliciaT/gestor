c557058dc9736acd56cbff556bfc1f86421aedc6
Glicia Tigre <gtrc@ecomp.poli.br>
2017-05-03 17:24:05 -0300

    Atualizado com La Treta

--- system/controllers/clients.php
+++ system/controllers/clients.php
@@ -8,27 +8,88 @@ class Clients extends Controller
 		$this->load_model('clients_model');
 	}
 	public function test(){
-		$cliente = array('id' => '10', 'name' => 'Vitória', 'ddd_1' => '81', 'tel_primary' => '111111111', 'ddd_2' => '81', 'tel_optional' => '222222222', 'email' => 'email@teste');
+		$cliente = array('name' => 'Vitória', 'associate_id'=> 10, 'email' => 'email@teste');
 		//$this->adicionarCliente($cliente);
-		$this->exibirCliente($cliente);
+		//$this->exibirCliente($cliente);
+		$var = array();
+		$this->adicionarCliente($cliente);
+		//$this->removerCliente($cliente);
+		//$this->exibirCliente($cliente);
 	}
 
 	public function adicionarCliente($data=""){
-		$this->model['clients_model']->insert(CLIENTS_NAME, $data);
-  }
+		if (is_null($data)) {
+			$this->return['success'] = FALSE;
+			$this->return['error'] .= "Null.";
+			return;
+		}
+		$status = $this->model['clients_model']->insert(CLIENTS_NAME, $data);
+			if(!$status){
+				$this->return['success'] = FALSE;
+				$this->return['error'] .= "Não foi possível inserir o Cliente.";
+				return;
+			}
+		}
+	}
 
 	public function alterarCliente(){
-		$this->model['clients_model']->update(CLIENTS_NAME,$data,"WHERE id = '" .$data['id']. "'");
-  }
+		if (is_null($data)) {
+			$this->return['success'] = FALSE;
+			$this->return['error'] .= "Null.";
+			return;
+		}
+		$status = $this->model['clients_model']->select(CLIENTS_NAME,"WHERE id='" . $data['id']. "'");
+		if(!$status){
+			$this->return['success'] = FALSE;
+			$this->return['error'] .= "Não foi possível selecionar o Cliente.";
+			return;
+		}else{
+			$status = $this->model['clients_model']->update(CLIENTS_NAME,$data,"WHERE id = '" .$data['id']. "'");
+			if (!$status) {
+				$this->return['success'] = FALSE;
+				$this->return['error'] .= "Não foi possível alterar o Cliente.";
+				return;
+			}
+		}
+	}
 
 	public function removerCliente(){
-		$this->model['clients_model']->delete(CLIENTS_NAME,$data,"WHERE id = '" .$data['id']. "'");
-  }
+		if (is_null($data)) {
+			$this->return['success'] = FALSE;
+			$this->return['error'] .= "Null.";
+			return;
+		}
+		$status = $this->model['clients_model']->select(CLIENTS_NAME,"WHERE id='" . $data['id']. "'");
+		if(!$status){
+			$this->return['success'] = FALSE;
+			$this->return['error'] .= "Não foi possível selecionar o Cliente.";
+			return;
+		}else{
+			$status = $this->model['clients_model']->delete(CLIENTS_NAME,$data,"WHERE id = '" .$data['id']. "'");
+			if (!$status) {
+				$this->return['success'] = FALSE;
+				$this->return['error'] .= "Não foi possível remover o Cliente.";
+				return;
+			}
+		}
+	}
 
 	public function exibirCliente($data=""){
-    $this->model['clients_model']->select(CLIENTS_NAME,"WHERE id='" . $data['id']. "'");
-      $cliente = $this->model['clients_model']->get_result();
-      var_dump($cliente);
-    }
+		if (is_null($data)) {
+			$this->return['success'] = FALSE;
+			$this->return['error'] .= "Null.";
+		}else{
+			$status = $this->model['clients_model']->select(CLIENTS_NAME,"WHERE id='" . $data['id']. "'");
+			$client = $this->model['clients_model']->get_result();
+			var_dump($client);
+			if(!$status){
+				$this->return['success'] = FALSE;
+				$this->return['error'] .= "Não foi possível exibir o Cliente.";
+				return;
+			}
+		}
+	}
+
+	//Depois de testar,  tentar evitar repetição de código com função para ver se é nulo e outra para ver se existe.
 }
 ?>
